d551b394662aa9b22a10295caebbdd4cf2883d03
Prakhar Parihar <166062387+PrakharParihar29@users.noreply.github.com>
2025-10-10 19:04:56 +0530

    ' error resolve

--- actions/useraction.js
+++ actions/useraction.js
@@ -17,7 +17,7 @@ export const initiate = async (amount, to_username, paymentform) => {
     instance.orders.create({
         amount: 50000,
         currency: "INR",
-        receipt: "receipt#1",
+        receipt: "receipt1",
         notes: {
             key1: "value3",
             key2: "value2"
--- app/page.js
+++ app/page.js
@@ -139,7 +139,7 @@ export default function Home() {
         <div className="mt-10 space-y-6">
           {[
             "At Get Me A Chai, we are dedicated to supporting developers, creators, and influencers by connecting them with their supporters. Our platform enables individuals to fund their projects and ideas, providing a space where creativity and innovation can thrive.",
-            "Our mission is to empower talented individuals by facilitating financial support, allowing them to focus on what they do best - creating. Whether you're a developer coding the next big app, a content creator making engaging videos, or an influencer sharing your passion, Get Me A Chai is here to help you achieve your goals.",
+            "Our mission is to empower talented individuals by facilitating financial support, allowing them to focus on what they do best - creating. Whether you&apos;re a developer coding the next big app, a content creator making engaging videos, or an influencer sharing your passion, Get Me A Chai is here to help you achieve your goals.",
             "We believe in the power of community and the impact of collective support. By providing a platform for patrons to contribute, we aim to transform dreams into reality and foster a culture of creativity and innovation.",
           ].map((text, idx) => (
             <p key={idx} className="text-gray-200 text-sm sm:text-base text-center font-sans px-2">
